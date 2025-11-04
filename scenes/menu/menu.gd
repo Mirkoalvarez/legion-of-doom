2be6bef1d260cbbccd41de59dc01a3063d65f0ea
Mirko Alvarez <mirko.bolivar2003@gmail.com>
2025-11-04 20:14:09 -0300

    Menu Principal
    menu control con conteiner y vbox, botones unique playbutton y quitbutton, playbutton abre escena main

---
+++ scenes/menu/menu.gd
@@ -0,0 +1,18 @@
+extends Control
+
+@onready var play_button: Button = %PlayButton
+@onready var quit_button: Button = %QuitButton
+
+const MAIN_SCENE_PATH := "res://scenes/main/main.tscn"
+
+func _ready() -> void:
+	play_button.pressed.connect(_on_play_pressed)
+	quit_button.pressed.connect(_on_quit_pressed)
+	# Opcional: mostrar el mouse
+	Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+
+func _on_play_pressed() -> void:
+	get_tree().change_scene_to_file(MAIN_SCENE_PATH)
+
+func _on_quit_pressed() -> void:
+	get_tree().quit()
